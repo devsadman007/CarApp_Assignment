6f32df35768a7d3383c78f39a91a9b7ace939824
sadman <ahsan.susmoy@gmail.com>
2025-08-20 23:09:51 +0600

    Simple Edit Done!

--- CarApp1/Car.cs
+++ CarApp1/Car.cs
@@ -9,16 +9,17 @@ namespace CarApp1
         public int Year { get; set; }
         public int Mileage { get; set; }
 
+        //This is Drive method
         public void Drive(int kilometers)
         {
             Mileage= Mileage + kilometers;
             Console.WriteLine("Car " + Brand + " " + Model + " has driven " + kilometers + " km. " + "Total mileage: " + Mileage + " km.");
         }
 
-
+        // This is ShowCarInfo method
         public void ShowCarInfo()
         {
-            Console.WriteLine("Car Info: " + " Brand " + Brand + " Model " + Model + " Year " + Year + " Mileage " + Mileage);
+            Console.WriteLine("Car Info:" + " Brand - " + Brand + "," + " Model - " + Model + "," + " Year - " + Year + "," + " Mileage - " + Mileage);
         }
     }
 }
